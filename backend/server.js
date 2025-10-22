d876a6f13dd7cc5e9cc9723d72fbec3f76aca7b9
Kitsune [Dev] <kitsuneidea@gmail.com>
2025-10-22 15:01:11 -0500

    Fix formatting of dotenv configuration

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Cargar variables de entorno
+dotenv.config();
 
 const app = express();
 
