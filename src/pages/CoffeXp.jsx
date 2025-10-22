bffdec33d49508d9121056d690f3c284ff214015
Kitsune [Dev] <kitsuneidea@gmail.com>
2025-10-22 15:03:01 -0500

    Fix import statement for useTranslation

--- src/pages/CoffeXp.jsx
+++ src/pages/CoffeXp.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useTranslation } from 'react-i18next'; // Importar useTranslation
+import { useTranslation } from 'react-i18next';
 import { initCustomFP } from "../utils/customfp.js";
 import { setupScrollAnimations } from "../utils/scrollAnimations.js";
 import "../styles/customfp.css";
